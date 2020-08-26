a583467bb4c2f060d4091f95169f556c40056ce7
studyskymate <studyskymate@gmail.com>
2020-08-26 16:38:56 +1200

    Hello World Added

---
+++ src/com/ceit/project/students/mesake/Hello.java
@@ -0,0 +1,10 @@
+package com.ceit.project.students.mesake;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/ceit/project/students/mohem/Hello.java
@@ -0,0 +1,10 @@
+package com.ceit.project.students.mohem;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
