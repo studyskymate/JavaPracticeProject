590ab7f8b09e67dc6801d8341cb1fea50a448937
studyskymate <studyskymate@gmail.com>
2020-08-26 17:36:15 +1200

    new code added

--- src/com/ceit/project/students/mohem/Hello.java
+++ src/com/ceit/project/students/mohem/Hello.java
@@ -1,9 +1,17 @@
 package com.ceit.project.students.mohem;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 	
+		float p,r,t;
+		
+		Scanner sc = new Scanner(System.in);
+		 p=sc.nextFloat();
+		 
+		 
 
 	}
 
