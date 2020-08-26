a9a33a0594cc5c56d4ddf4a82d1d086081b37afa
Mesake N. Masi <S2008005080@student.fnu.ac.fj>
2020-08-26 17:43:13 +1200

    Amendments

--- src/com/ceit/project/students/mesake/Hello.java
+++ src/com/ceit/project/students/mesake/Hello.java
@@ -1,12 +1,37 @@
 package com.ceit.project.students.mesake;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Mesake");
+	
+		System.out.println("Hello Mesake");
+	
+		float p,r,t,si;
+		
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter Principle");
+		
+		p=sc.nextFloat();
+		
+		System.out.println("Enter Interest Rate");
+		r=sc.nextFloat();
+		
+		System.out.println("Enter Time");
+		t=sc.nextFloat();
+		
+		si=(p*r*t)/100;
+		
+		System.out.println("The Simple Interest is:"+si);
+		
+		
 		
 
 	}
 
 }
+
+  
